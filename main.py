ba288427b7a38702b5deadc5d7028a8e85279751
mvickm2 <victoria.munafo@infolytics.ai>
2023-10-15 12:02:52 -0300

    Print function changed by logging in main

--- main.py
+++ main.py
@@ -3,46 +3,48 @@ from src.solutions import *
 import click
 import json
 import time
+import logging
 
-# Generical function to print results
-def print_results_generic(results, num_results=10):
-    print("Results:")
+# Configure the logging
+logging.basicConfig(level=logging.INFO)  # Set the desired logging level
+
+# Generical function to log results
+def log_results_generic(results, num_results=10):
+    logging.info("Results:")
     for i, result in enumerate(results[:num_results], 1):
-        print(f"Result {i}: {result}")
+        logging.info(f"Result {i}: {result}")
 
-# Function to execute and print execution status
-def execute_and_print(function, message_time, message_memory, file_path, print_function):
-    print(f"Executing {message_time}...")
+# Function to execute and log execution status
+def execute_and_log(function, message_time, message_memory, file_path, log_function):
+    logging.info(f"Executing {message_time}...")
     start_time = time.time()
     result = function(file_path)
     execution_time = time.time() - start_time
-    print(f"{message_time} executed in {execution_time} seconds.")
+    logging.info(f"{message_time} executed in {execution_time} seconds.")
 
-    print(f"Executing {message_memory} with memory profiling...")
+    logging.info(f"Executing {message_memory} with memory profiling...")
     start_time = time.time()
     result = function(file_path)
     execution_time = time.time() - start_time
-    print(f"{message_memory} executed in {execution_time} seconds.")
+    logging.info(f"{message_memory} executed in {execution_time} seconds.")
 
-    print("Wait for memory profiling")
+    logging.info("Wait for memory profiling")
     time.sleep(2)
-    # Call to print results
-    print_function(result)
-    
+    # Call to log results
+    log_function(result)
+
 # Main function
 @click.command()
 @click.option("--file_path", help="Path of JSON file")
-
 def main(file_path):
-    
     # q1 execution
-    execute_and_print(q1_time, "q1_time", "q1_memory", file_path, print_results_generic)
+    execute_and_log(q1_time, "q1_time", "q1_memory", file_path, log_results_generic)
 
     # q2 execution
-    execute_and_print(q2_time, "q2_time", "q2_memory", file_path, print_results_generic)
+    execute_and_log(q2_time, "q2_time", "q2_memory", file_path, log_results_generic)
 
     # q3 execution
-    execute_and_print(q3_time, "q3_time", "q3_memory", file_path, print_results_generic)
+    execute_and_log(q3_time, "q3_time", "q3_memory", file_path, log_results_generic)
 
 if __name__ == "__main__":
     main()
